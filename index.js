4549eab48d3f6eacd524c06393441dcefec65b2d
noob247365 <noob247365@gmail.com>
2022-01-26 13:08:27 -0500

    Refactor to make sure the spotify endpoint works

--- index.js
+++ index.js
@@ -5,27 +5,12 @@ const port = process.env.PORT ?? 3000;
 const codes = {};
 
 app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/:guid', (req, res) => {
-    if (!codes.hasOwnProperty(req.params.guid)) {
-        res.json({ success: false, message: `Unknown ID: '${req.params.guid}'` });
-        return;
-    }
-    res.json({ success: true, result: codes[req.params.guid] });
-});
 app.get('/:guid/create', (req, res) => {
     if (codes.hasOwnProperty(req.params.guid)) {
         res.json({ success: false, message: `Already present in cache: '${req.params.guid}'` });
         return;
     }
     codes[req.params.guid] = null;
-    res.json({ success: true });
-});
-app.get('/:guid/callback', (req, res) => {
-    if (!codes.hasOwnProperty(req.params.guid)) {
-        res.json({ success: false, message: `Unknown ID: '${req.params.guid}'\n\nQuery:\n${JSON.stringify(req.query, null, '  ')}` });
-        return;
-    }
-    codes[req.params.guid] = JSON.parse(JSON.stringify(req.query));
     res.json({ success: true });
 });
 app.get('/:guid/delete', (req, res) => {
@@ -35,6 +20,13 @@ app.get('/:guid/delete', (req, res) => {
     }
     delete codes[req.params.guid];
     res.json({ success: true });
+});
+app.get('/:guid/view', (req, res) => {
+    if (!codes.hasOwnProperty(req.params.guid)) {
+        res.json({ success: false, message: `Unknown ID: '${req.params.guid}'` });
+        return;
+    }
+    res.json({ success: true, result: codes[req.params.guid] });
 });
 
 app.get('/spotify-callback', (req, res) => {
